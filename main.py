7662208f2fdb3afda683ffc5f1abaa81fe8d4d3b
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-11-11 21:17:34 +0200

    Import all models and load env

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import SQLModel, create_engine
+from models.newsletter import Newsletter
+from models.user import User
+from models.comment import Comment
+from models.post import Post
+from models.subscription import Subscription
+from dotenv import load_dotenv
+import os
 
-class Newsletter(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+load_dotenv()
 
-sqlite_file = "test.db"
-sqlite_url = f"sqlite:///{sqlite_file}"
-engine = create_engine(sqlite_url)
+postgres_url = os.getenv("POSTGRES_URL")
+engine = create_engine(postgres_url)
 
 def create_tables():
     SQLModel.metadata.create_all(engine)
