95d86dee3ddf7f713b1477cde077279a4c6f40c4
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-11-12 21:09:27 +0200

    Randomly generate uuid in the user model

--- models/user.py
+++ models/user.py
@@ -2,10 +2,13 @@ from sqlmodel import Field, Session, SQLModel, create_engine, select
 import uuid
 from datetime import datetime
 
+def generate_uuid():
+    return str(uuid.uuid4())
+
 class User(SQLModel, table=True):
-    id: uuid.UUID = Field(default=uuid.uuid4(), primary_key=True, unique=True)
+    id: uuid.UUID = Field(default_factory=generate_uuid, primary_key=True, unique=True)
     first_name: str = Field(max_length=50)
     last_name: str = Field(max_length=50)
     email: str = Field(max_length=80)
     created_at: datetime = Field(default=datetime.now())
-    updated_at: datetime = Field(default=None)
+    updated_at: datetime = Field(default=datetime.now())
