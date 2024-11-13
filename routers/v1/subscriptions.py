2778015e587a418ef100d080ffba08e9805fe510
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-11-13 13:36:35 +0200

    Correctly fetch subscriptions for a newsletter

--- routers/v1/subscriptions.py
+++ routers/v1/subscriptions.py
@@ -16,7 +16,11 @@ router = APIRouter(
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
-@router.get("/{user_id}")
-async def get_all_subscriptions(user_id: int, session: SessionDep):
-    subscriptions = session.exec(select(Subscription).where(Subscription.subscriber == user_id).limit(10)).all()
+@router.get("/{newsletter_id}")
+async def get_all_subscriptions_for_newsletter(newsletter_id: int, session: SessionDep):
+    subscriptions = session.exec(select(Subscription).where(Subscription.newsletter == newsletter_id).limit(10)).all()
+
+    if subscriptions is None:
+        raise HTTPException(status_code=404, detail='No subscriptions')
+
     return subscriptions
