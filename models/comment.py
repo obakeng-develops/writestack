35941219e30b8959937e1ef7e538177260d51fcf
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-11-13 13:34:58 +0200

    Add foreign key association to post on Comments model

--- models/comment.py
+++ models/comment.py
@@ -6,4 +6,5 @@ from helpers.generate_uuid import generate_uuid
 class Comment(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=generate_uuid, primary_key=True, unique=True)
     body: str
+    post: uuid.UUID = Field(foreign_key='post.id')
     user: uuid.UUID = Field(foreign_key='user.id')
