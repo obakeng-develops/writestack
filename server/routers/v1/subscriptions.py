9280deee82673700179e6aee17cf409a11529245
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-11-17 19:12:30 +0200

    Fix router path name, return a list of subscriptions, correctly validate the result of fetching subscriptions

--- server/routers/v1/subscriptions.py
+++ server/routers/v1/subscriptions.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 from helpers.database import get_session
 from models.subscription import Subscription
-from typing import Annotated
+from typing import Annotated, List
 import uuid
 
 router = APIRouter(
@@ -17,11 +17,11 @@ router = APIRouter(
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
-@router.get("/{newsletter_uuid}")
-async def get_all_subscriptions_for_newsletter(newsletter_uuid: uuid.UUID, session: SessionDep):
+@router.get("/newsletter/{newsletter_uuid}")
+async def get_all_subscriptions_for_newsletter(newsletter_uuid: uuid.UUID, session: SessionDep) -> List[Subscription]:
     subscriptions = session.exec(select(Subscription).where(Subscription.newsletter == newsletter_uuid).limit(10)).all()
 
-    if subscriptions is None:
-        raise HTTPException(status_code=404, detail='No subscriptions')
+    if not subscriptions:
+        raise HTTPException(status_code=404, detail='No subscriptions found for the given newsletter')
 
     return subscriptions
