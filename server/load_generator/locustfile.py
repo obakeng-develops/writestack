88ac26bcac87c4a55d1e3e56a5021b6467a6c614
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-11-17 19:53:09 +0200

    Add load tests for Post

--- server/load_generator/locustfile.py
+++ server/load_generator/locustfile.py
@@ -7,6 +7,9 @@ import json
 with open("user_payloads.json", "r") as f:
     user_payloads = json.load(f)
     
+with open("posts_payloads.json", "r") as f:
+    post_payloads = json.load(f)
+    
 def get_client_response(resp: ResponseContextManager):
     if resp.status_code == 200:
         resp.success()
@@ -30,4 +33,23 @@ class User(HttpUser):
     @task
     def get_newsletters_for_users(self):
         with self.client.get("/users/newsletters/{user_uuid}", payload=self.payload, catch_response=True) as response:
+            get_client_response(response)
+
+class Post(HttpUser):
+    wait_time = between(1, 5)
+    payload = random.choice(post_payloads)
+    
+    @task
+    def create_post(self):
+        with self.client.post("/posts", json=self.payload, catch_response=True) as response:
+            get_client_response(response)
+            
+    @task
+    def get_post(self):
+        with self.client.get("/posts/{post_uuid}", json=self.payload, catch_response=True) as response:
+            get_client_response(response)
+            
+    @task
+    def get_all_comments_for_post(self):
+        with self.client.get("/posts/comments/{post_uuid}", json=self.payload, catch_response=True) as response:
             get_client_response(response)
