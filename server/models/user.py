b859d6f56a32fbc4c70d9283f1acfa1dec37a447
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-11-22 16:52:34 +0200

    Update User models to better represent responses

--- server/models/user.py
+++ server/models/user.py
@@ -3,10 +3,23 @@ import uuid
 from datetime import datetime
 from helpers.generate_uuid import generate_uuid
 
-class User(SQLModel, table=True):
-    id: uuid.UUID = Field(default_factory=generate_uuid, primary_key=True, unique=True)
+class UserBase(SQLModel):
     first_name: str = Field(max_length=50)
     last_name: str = Field(max_length=50)
     email: str = Field(max_length=80)
-    created_at: datetime = Field(default=datetime.now())
+
+class User(UserBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, unique=True)
+    created_at: datetime = Field(default=datetime.today())
     updated_at: datetime = Field(default=datetime.now())
+
+class UserCreate(UserBase):
+    pass
+
+class UserPublic(UserBase):
+    id: uuid.UUID
+
+class UserUpdate(SQLModel):
+    first_name: str | None = None
+    last_name: str | None = None
+    email: str | None = None
