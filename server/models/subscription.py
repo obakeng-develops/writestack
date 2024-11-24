6d1522f3851ee07f020b72c935823f61d61b0b1f
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-11-24 16:28:06 +0200

    Use the subscription base model correctly

--- server/models/subscription.py
+++ server/models/subscription.py
@@ -4,13 +4,13 @@ from datetime import datetime
 from helpers.generate_uuid import generate_uuid
 
 class SubscriptionBase(SQLModel):
-    subcriber: uuid.UUID
-    newsletter: uuid.UUID
+    subscriber: uuid.UUID = Field(foreign_key='user.id')
+    newsletter: uuid.UUID = Field(foreign_key='newsletter.id')
 
 class Subscription(SubscriptionBase, table=True):
     id: uuid.UUID = Field(default_factory=generate_uuid, primary_key=True, unique=True)
-    subscriber: uuid.UUID = Field(foreign_key='user.id')
-    newsletter: uuid.UUID = Field(foreign_key='newsletter.id')
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: datetime = Field(default=None) 
 
 class SubscriptionCreate(SubscriptionBase):
     pass
