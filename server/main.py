6a780a3ae651c9ed70d2cf5c7b4daf167af0c4c0
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-11-24 17:03:11 +0200

    Include comments router to main file

--- server/main.py
+++ server/main.py
@@ -5,7 +5,7 @@ from models.comment import Comment
 from models.post import Post
 from models.subscription import Subscription
 from dotenv import load_dotenv
-from routers.v1 import newsletter, user, post, subscriptions
+from routers.v1 import newsletter, user, post, subscriptions, comment
 from helpers.database import engine, create_tables
 from prometheus_fastapi_instrumentator import Instrumentator
 import os
@@ -25,3 +25,4 @@ app.include_router(newsletter.router)
 app.include_router(user.router)
 app.include_router(post.router)
 app.include_router(subscriptions.router)
+app.include_router(comment.router)
