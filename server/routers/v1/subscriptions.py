3cc27459638a9f5c6be54014371d0816f3ec0e06
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-11-24 16:26:01 +0200

    Update subscription routes to use public response model

--- server/routers/v1/subscriptions.py
+++ server/routers/v1/subscriptions.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 from helpers.database import get_session
-from models.subscription import Subscription
+from models.subscription import Subscription, SubscriptionPublic
 from typing import Annotated, List
 import uuid
 
@@ -17,7 +17,7 @@ router = APIRouter(
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
-@router.get("/newsletter/{newsletter_uuid}")
+@router.get("/newsletter/{newsletter_uuid}", response_model=SubscriptionPublic)
 async def get_all_subscriptions_for_newsletter(newsletter_uuid: uuid.UUID, session: SessionDep) -> List[Subscription]:
     subscriptions = session.exec(select(Subscription).where(Subscription.newsletter == newsletter_uuid).limit(10)).all()
 
