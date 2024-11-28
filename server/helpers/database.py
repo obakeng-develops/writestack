ed4d685b501a719d3d578c20b5ee4b756d622b98
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-11-28 16:27:19 +0200

    Update queuepoolsize explicitly

--- server/helpers/database.py
+++ server/helpers/database.py
@@ -2,7 +2,7 @@ from sqlmodel import create_engine, Session, SQLModel
 import os
 
 postgres_url = "sqlite:///writestack.db"
-engine = create_engine(postgres_url)
+engine = create_engine(postgres_url, pool_size=10, max_overflow=30)
 
 def create_tables():
     SQLModel.metadata.create_all(engine)
