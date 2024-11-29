e03812e944016cdcb9aeef99f9a3ebbc12c35c5f
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-11-29 11:46:01 +0200

    Add some test code

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="container mx-auto px-5">
+    <div class="flex flex-col m-4">
+        <button class="bg-sky-500 text-white p-3 rounded">New post</button>
+        <div class="my-3">
+            <h2 class="text-4xl">adipisicing</h2>
+            <h3 class="text-lg">Ullamco aliqua exercitation veniam incididunt ad commodo aute nulla occaecat mollit minim eiusmod quis.</h3>
+            <p class="truncate">Qui ad ex duis ex cupidatat do. Est ex occaecat Lorem adipisicing tempor aliquip aute sint ipsum veniam proident Lorem proident exercitation. Consectetur minim in aliquip esse aute est. Sint adipisicing Lorem nulla magna consequat. Nisi id ex adipisicing esse sint non.</p>
+        </div>
+        <div class="my-3">
+            <h2 class="text-4xl">sint</h2>
+            <h3 class="text-lg">Magna veniam laborum non proident amet ea officia fugiat mollit.</h3>
+            <p class="truncate">Officia deserunt ut cillum in duis exercitation fugiat. Consequat non nulla non incididunt mollit pariatur. Do in et tempor non ad ex deserunt ipsum ut in.</p>
+        </div>
+        <div class="my-3">
+            <h2 class="text-4xl">laboris</h2>
+            <h3 class="text-lg">Nostrud aliqua labore id occaecat nisi qui in.</h3>
+            <p class="truncate">Ex pariatur non cupidatat Lorem. Duis do laborum nulla magna incididunt proident. Aliquip aliqua consectetur qui magna sint non velit aute velit. Sunt dolore mollit consequat aute nisi. Ea quis laboris enim quis laborum ut nisi sunt. In qui occaecat incididunt mollit duis cillum eiusmod sit. Mollit laborum consectetur voluptate officia elit sit laborum voluptate esse labore aliquip reprehenderit enim adipisicing.</p>
+        </div>
+    </div>
+</div>
