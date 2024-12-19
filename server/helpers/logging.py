4062962ca4444824edf877f5ece40c3106682d3f
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-12-19 13:27:12 +0200

    Add a global logger for static information

--- server/helpers/logging.py
+++ server/helpers/logging.py
@@ -1,6 +1,7 @@
 import structlog
 import fastapi
 import sys
+from fastapi import Request
 
 # configure structlog
 structlog.configure(
@@ -13,5 +14,8 @@ structlog.configure(
 )
 
 logger = structlog.get_logger()
+
 python_version = f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
 fastapi_version = fastapi.__version__
+
+global_logger = logger.bind(py_version=python_version, fastapi_version=fastapi_version, environment="production", service="writestack")
