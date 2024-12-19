e6b0b09b44a104c00621c93056e2129104c6dfb5
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-12-19 12:53:24 +0200

    Update logging on the user routes

--- server/routers/v1/user.py
+++ server/routers/v1/user.py
@@ -28,65 +28,68 @@ SessionDep = Annotated[Session, Depends(get_session)]
 @router.post("/", response_model=UserPublic, status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserCreate, request: Request, session: SessionDep) -> User:
     user_agent = request.headers.get('User-Agent')
+    user_logger = logger.bind(device_type=user_agent, py_version=python_version, fastapi_version=fastapi_version, environment="production", service="writestack", http_scheme=request.url.scheme, route=request.url.path, method=request.method)
     
-    logger.debug("Validating user data...")
+    user_logger.debug("user.creation.validation.started")
     new_user = User.model_validate(user)
-    logger.debug("Validation complete.")
+    user_logger.debug("user.creation.validation.success")
+    user_logger.info("user.creation.database_commit.started", status_code=201)
     session.add(new_user)
-    logger.info("Adding new user", email=new_user.email, created_at=new_user.created_at, first_name=new_user.first_name, last_name=new_user.last_name, route=request.url.path, status_code=201, method=request.method, device_type=user_agent, py_version=python_version, fastapi_version=fastapi_version)
+    user_logger.info("user.creation.database_commit.success", user_id=new_user.id)
     session.commit()
-    logger.info("Committing user to the database.")
+    user_logger.info("user.creation.success")
     session.refresh(new_user)
-    logger.info("Refreshing user model")
     return new_user
 
 @router.get("/{user_uuid}", response_model=UserPublic, status_code=status.HTTP_200_OK)
 async def get_user(user_uuid: uuid.UUID, request: Request, session: SessionDep) -> User:
     user_agent = request.headers.get('User-Agent')
+    user_logger = logger.bind(device_type=user_agent, py_version=python_version, fastapi_version=fastapi_version, environment="production", service="writestack", http_scheme=request.url.scheme, route=request.url.path, method=request.method, user_id=str(user_uuid))
     
-    logger.debug("Searching for user with ID %s", user_uuid)
+    user_logger.debug("user.search.started")
     user = session.exec(select(User).where(User.id == user_uuid)).first()
 
     if not user:
-        logger.exception("User ID %s not found", user_uuid, route=request.url.path, status_code=404, method=request.method)
+        user_logger.exception("user.search.failed", detail="User not found", status_code=404)
         raise HTTPException(status_code=404, detail='User not found')
     
-    logger.info("User found", email=user.email, first_name=user.first_name, last_name=user.last_name, route=request.url.path, method=request.method, status_code=200, device_type=user_agent, py_version=python_version, fastapi_version=fastapi_version)
+    user_logger.info("user.search.success", status_code=200)
     return user
 
 @router.patch("/{user_uuid}", response_model=UserPublic, status_code=status.HTTP_200_OK)
 async def update_user(user_uuid: uuid.UUID, updated_user: UserUpdate, request: Request, session: SessionDep) -> User:
     user_agent = request.headers.get('User-Agent')
+    user_logger = logger.bind(device_type=user_agent, py_version=python_version, fastapi_version=fastapi_version, environment="production", service="writestack", http_scheme=request.url.scheme, route=request.url.path, method=request.method, user_id=str(user_uuid))
     
-    logger.debug("Searching for user with ID %s", user_uuid)
+    user_logger.debug("user.search.started")
     user = session.exec(select(User).where(User.id == user_uuid)).first()
 
     if not user:
-        logger.exception("User ID %s not found", user_uuid, router=request.url.path, status_code=404, method=request.method, device_type=user_agent)
+        user_logger.exception("user.search.failed", detail="User not found", status_code=404)
         raise HTTPException(status_code=404, detail='User not found')
     
-    logger.info("Updating user %s", user.email)
+    user_logger.info("user.update.data_received")
     user_data = updated_user.model_dump(exclude_unset=True)
     user.sqlmodel_update(user_data)
-    logger.info("Update completed.")
-    logger.info("Adding user", email=user.email, first_name=user.first_name, last_name=user.last_name, route=request.url.path, method=request.method, status_code=200, device_type=user_agent, py_version=python_version, fastapi_version=fastapi_version)
+    user_logger.info("user.update.database_commit.started", status_code=200)
     session.add(user)
-    logger.info("User added")
+    user_logger.info("user.update.database_commit.complete")
     session.commit()
-    logger.info("User committed")
+    user_logger.info("user.update.success")
     session.refresh(user)
     return user
 
-@router.get("/newsletters/{user_uuid}", response_model=List[NewsletterPublic], status_code=status.HTTP_200_OK)
+@router.get("/{user_uuid}/newsletters", response_model=List[NewsletterPublic], status_code=status.HTTP_200_OK)
 async def get_newsletters_for_users(user_uuid: uuid.UUID, request: Request, session: SessionDep) -> List[Newsletter]:
     user_agent = request.headers.get('User-Agent')
+    user_logger = logger.bind(device_type=user_agent, py_version=python_version, fastapi_version=fastapi_version, environment="production", service="writestack", http_scheme=request.url.scheme, route=request.url.path, method=request.method, user_id=str(user_uuid))
     
-    logger.debug("Searching for newsletters for user with ID %s", user_uuid)
+    user_logger.debug("newsletter.search.started")
     newsletters = session.exec(select(Newsletter).where(Newsletter.user == user_uuid)).all()
 
     if not newsletters:
-        logger.exception("Newsletters not found for user %s", user_uuid, router=request.url.path, status_code=404, method=request.method, device_type=user_agent, py_version=python_version, fastapi_version=fastapi_version)
+        user_logger.exception("newsletter.search.failed", detail="User not found", status_code=404)
         raise HTTPException(status_code=404, detail='No newsletters found')
 
-    logger.info("Newsletters found", route=request.url.path, status_code=200, method=request.method, device_type=user_agent, py_version=python_version, fastapi_version=fastapi_version)
+    user_logger.info("newsletter.search.success", status_code=200)
     return newsletters
