9d1f26fb9cfbf70f2189f5911055b851403b51bb
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2025-01-19 17:45:25 +0200

    Add new log fields for newsletter model data

--- server/routers/v1/newsletter.py
+++ server/routers/v1/newsletter.py
@@ -32,7 +32,7 @@ async def get_newsletter(newsletter_uuid: uuid.UUID, request: Request, session: 
         newsletter_logger.error("newsletter.search.failed", detail="Newsletter not found", status_code=404)
         raise HTTPException(status_code=404, detail='Newsletter not found')
     
-    newsletter_logger.info("newsletter.search.success", detail="Newsletter found", status_code=200)
+    newsletter_logger.info("newsletter.search.success", detail="Newsletter found", status_code=200, newsletter=newsletter.name)
     return newsletter
 
 @router.get("/posts/{newsletter_uuid}", response_model=List[PostPublic], status_code=status.HTTP_200_OK)
@@ -47,8 +47,10 @@ async def get_posts_by_newsletter(newsletter_uuid: uuid.UUID, request: Request, 
     if not posts:
         newsletter_logger.error("newsletter.search.failed", detail="No posts found", status_code=404)
         raise HTTPException(status_code=404, details='There are no posts')
+    
+    post_data = [(post.subtitle, post.published, post.newsletter) for post in posts]
 
-    newsletter_logger.info("newsletter.search.status", detail="Posts found", status_code=200)
+    newsletter_logger.info("newsletter.search.status", detail="Posts found", status_code=200, posts=post_data)
     return posts
 
 @router.patch("/{newsletter_uuid}", response_model=NewsletterPublic, status_code=status.HTTP_200_OK)
@@ -72,7 +74,7 @@ async def update_newsletter(newsletter_uuid: uuid.UUID, updated_newsletter: News
     session.add(newsletter)
     newsletter_logger.info("newsletter.update.database_commit.success")
     session.commit()
-    newsletter_logger.info("newsletter.update.success", detail="Newsletter updated", status_code=200)
+    newsletter_logger.info("newsletter.update.success", detail="Newsletter updated", status_code=200, newsletter=newsletter.name)
     session.refresh(newsletter)
     return newsletter
 
@@ -89,6 +91,6 @@ async def create_newsletter(newsletter: NewsletterCreate, request: Request, sess
     session.add(create_newsletter)
     newsletter_logger.info("newsletter.creation.database_commit.success")
     session.commit()
-    newsletter_logger.info("newsletter.creation.success", detail="Newsletter created", newsletter_id=str(create_newsletter.id), status_code=201)
+    newsletter_logger.info("newsletter.creation.success", detail="Newsletter created", newsletter_id=str(create_newsletter.id), status_code=201, newsletter=create_newsletter.name)
     session.refresh(create_newsletter)
     return create_newsletter
