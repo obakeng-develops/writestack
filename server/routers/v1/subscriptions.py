b9f9b59c7c28713f8c1f8980f6fc88d90d029896
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2025-02-05 14:56:44 +0200

    Add routes to create and get subscription

--- server/routers/v1/subscriptions.py
+++ server/routers/v1/subscriptions.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session, select
 from helpers.database import get_session
-from models.subscription import Subscription, SubscriptionPublic
+from models.subscription import Subscription, SubscriptionPublic, SubscriptionCreate
 from typing import Annotated, List
 import uuid
 
@@ -17,7 +17,7 @@ router = APIRouter(
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
-@router.get("/newsletter/{newsletter_uuid}", response_model=List[SubscriptionPublic], status_code=status.HTTP_200_OK)
+@router.get("/{newsletter_uuid}", response_model=List[SubscriptionPublic], status_code=status.HTTP_200_OK)
 async def get_all_subscriptions_for_newsletter(newsletter_uuid: uuid.UUID, session: SessionDep) -> List[Subscription]:
     subscriptions = session.exec(select(Subscription).where(Subscription.newsletter == newsletter_uuid).limit(10)).all()
 
@@ -25,3 +25,20 @@ async def get_all_subscriptions_for_newsletter(newsletter_uuid: uuid.UUID, sessi
         raise HTTPException(status_code=404, detail='No subscriptions found for the given newsletter')
 
     return subscriptions
+
+@router.post("/", response_model=SubscriptionPublic, status_code=status.HTTP_200_OK)
+async def create_subscription(subscription: SubscriptionCreate, session: SessionDep) -> Subscription:
+    new_subscription = Subscription.model_validate(subscription)
+    session.add(new_subscription)
+    session.commit()
+    session.refresh(new_subscription)
+    return new_subscription
+
+@router.get("/{subscription_uuid}", response_model=SubscriptionPublic, status_code=status.HTTP_200_OK)
+async def get_subscription(subscription_uuid: uuid.UUID, session: SessionDep) -> Subscription:
+    subscription = session.exec(select(Subscription).where(Subscription.id == subscription_uuid)).first()
+
+    if not subscription:
+        raise HTTPException(status_code=404, detail='Subscription not found')
+    
+    return subscription
